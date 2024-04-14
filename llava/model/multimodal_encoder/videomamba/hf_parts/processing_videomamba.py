9c12e14cc977e696cfcc9aec6e2cd34e72a00109
deepbuzin <andrew@tensorsense.com>
2024-04-14 13:07:25 +0000

    Outline VideoMamba vision tower components

---
+++ llava/model/multimodal_encoder/videomamba/hf_parts/processing_videomamba.py
@@ -0,0 +1,203 @@
+from transformers import ProcessorMixin
+from torchvision import transforms
+import decord
+from decord import VideoReader
+import torch
+import numpy as np
+import random
+
+decord.bridge.set_bridge("torch")
+
+mean = (0.48145466, 0.4578275, 0.40821073)
+std = (0.26862954, 0.26130258, 0.27577711)
+
+normalize = transforms.Normalize(mean, std)
+
+# loaded images and videos are torch.Tensor of torch.uint8 format,
+# ordered as (T, 1 or 3, H, W) where T=1 for image
+type_transform = transforms.Lambda(lambda x: x.float().div(255.0))
+
+transform = transforms.Compose(
+    [
+        transforms.Resize(
+            (config.inputs.image_res, config.inputs.image_res),
+            interpolation=transforms.InterpolationMode.BICUBIC,
+        ),
+        type_transform,
+        normalize,
+    ]
+)
+
+video_reader = read_frames_decord
+
+max_num_frames = -1
+frames, frame_indices, video_duration = video_reader(
+    "/data/vlm_sandbox/videos/lie1.mp4",
+    config.inputs.video_input.num_frames,
+    config.inputs.video_input.sample_type,
+    max_num_frames=max_num_frames,
+    client=None,
+    trimmed30=False,
+)
+
+frames = transform(frames).unsqueeze(0)
+frames.shape
+
+
+def make_list_of_images(x):
+    if not isinstance(x, list):
+        return [x]
+    return x
+
+
+def get_frame_indices(
+    num_frames, vlen, sample="rand", fix_start=None, input_fps=1, max_num_frames=-1
+):
+    if sample in ["rand", "middle"]:  # uniform sampling
+        acc_samples = min(num_frames, vlen)
+        # split the video into `acc_samples` intervals, and sample from each interval.
+        intervals = np.linspace(start=0, stop=vlen, num=acc_samples + 1).astype(int)
+        ranges = []
+        for idx, interv in enumerate(intervals[:-1]):
+            ranges.append((interv, intervals[idx + 1] - 1))
+        if sample == "rand":
+            try:
+                frame_indices = [random.choice(range(x[0], x[1])) for x in ranges]
+            except:
+                frame_indices = np.random.permutation(vlen)[:acc_samples]
+                frame_indices.sort()
+                frame_indices = list(frame_indices)
+        elif fix_start is not None:
+            frame_indices = [x[0] + fix_start for x in ranges]
+        elif sample == "middle":
+            frame_indices = [(x[0] + x[1]) // 2 for x in ranges]
+        else:
+            raise NotImplementedError
+
+        if len(frame_indices) < num_frames:  # padded with last frame
+            padded_frame_indices = [frame_indices[-1]] * num_frames
+            padded_frame_indices[: len(frame_indices)] = frame_indices
+            frame_indices = padded_frame_indices
+    elif "fps" in sample:  # fps0.5, sequentially sample frames at 0.5 fps
+        output_fps = float(sample[3:])
+        duration = float(vlen) / input_fps
+        delta = (
+            1 / output_fps
+        )  # gap between frames, this is also the clip length each frame represents
+        frame_seconds = np.arange(0 + delta / 2, duration + delta / 2, delta)
+        frame_indices = np.around(frame_seconds * input_fps).astype(int)
+        frame_indices = [e for e in frame_indices if e < vlen]
+        if max_num_frames > 0 and len(frame_indices) > max_num_frames:
+            frame_indices = frame_indices[:max_num_frames]
+            # frame_indices = np.linspace(0 + delta / 2, duration + delta / 2, endpoint=False, num=max_num_frames)
+    else:
+        raise ValueError
+    return frame_indices
+
+
+def read_frames_decord(
+    video_path,
+    num_frames,
+    sample="rand",
+    fix_start=None,
+    max_num_frames=-1,
+    client=None,
+    trimmed30=False,
+):
+    if "s3://" in video_path:
+        video_bytes = client.get(video_path)
+        video_reader = VideoReader(io.BytesIO(video_bytes), num_threads=1)
+    else:
+        video_reader = VideoReader(video_path, num_threads=1)
+    vlen = len(video_reader)
+    fps = video_reader.get_avg_fps()
+    duration = vlen / float(fps)
+
+    # only use top 30 seconds
+    if trimmed30 and duration > 30:
+        duration = 30
+        vlen = int(30 * float(fps))
+
+    frame_indices = get_frame_indices(
+        num_frames,
+        vlen,
+        sample=sample,
+        fix_start=fix_start,
+        input_fps=fps,
+        max_num_frames=max_num_frames,
+    )
+    frames = video_reader.get_batch(frame_indices)  # (T, H, W, C), torch.uint8
+    frames = frames.permute(0, 3, 1, 2)  # (T, C, H, W), torch.uint8
+    return frames, frame_indices, duration
+
+
+class VideoMambaVideoProcessor(ProcessorMixin):
+    attributes = []
+    tokenizer_class = "VideoMambaVideoProcessor"
+
+    def __init__(self, config, tokenizer=None, **kwargs):
+        super().__init__(**kwargs)
+        self.config = config
+        self.transform = get_video_transform(config)
+        self.image_processor = load_and_transform_video
+        self.tokenizer = tokenizer
+
+    def __call__(
+        self, images=None, text=None, context_length=77, return_tensors=None, **kwargs
+    ):
+        if text is None and images is None:
+            raise ValueError(
+                "You have to specify either text or images. Both cannot be none."
+            )
+
+        if text is not None:
+            encoding = self.tokenizer(
+                text,
+                max_length=context_length,
+                padding="max_length",
+                truncation=True,
+                return_tensors=return_tensors,
+                **kwargs
+            )
+
+        if images is not None:
+            images = make_list_of_images(images)
+            image_features = [
+                self.image_processor(
+                    image,
+                    self.transform,
+                    video_decode_backend=self.config.vision_config.video_decode_backend,
+                    num_frames=self.config.vision_config.num_frames,
+                )
+                for image in images
+            ]
+            image_features = torch.stack(image_features)
+
+        if text is not None and images is not None:
+            encoding["pixel_values"] = image_features
+            return encoding
+        elif text is not None:
+            return encoding
+        else:
+            return {"pixel_values": image_features}
+
+    def preprocess(self, images, return_tensors):
+        return self.__call__(images=images, return_tensors=return_tensors)
+
+    def batch_decode(self, skip_special_tokens=True, *args, **kwargs):
+        """
+        This method forwards all its arguments to CLIPTokenizerFast's [`~PreTrainedTokenizer.batch_decode`]. Please
+        refer to the docstring of this method for more information.
+        """
+        return self.tokenizer.batch_decode(
+            *args, skip_special_tokens=skip_special_tokens, **kwargs
+        )
+
+    def decode(self, skip_special_tokens=True, *args, **kwargs):
+        """
+        This method forwards all its arguments to CLIPTokenizerFast's [`~PreTrainedTokenizer.decode`]. Please refer to
+        the docstring of this method for more information.
+        """
+        return self.tokenizer.decode(
+            *args, skip_special_tokens=skip_special_tokens, **kwargs
+        )
---
+++ llava/model/multimodal_encoder/videomamba/vision_tower.py
@@ -0,0 +1,197 @@
+import torch
+import torch.nn as nn
+
+from transformers import CLIPVisionModel, CLIPImageProcessor, CLIPVisionConfig
+from hf_parts.processing_videomamba import VideoMambaVideoProcessor
+from models.umt_videomamba import UMT_VIDEOMAMBA
+from models.backbones.bert.tokenization_bert import BertTokenizer
+
+
+class VideoMambaVisionTower(nn.Module):
+    def __init__(self, vision_tower, args, delay_load=False):
+        super().__init__()
+
+        self.is_loaded = False
+
+        self.vision_tower_name = vision_tower
+        self.select_layer = args.mm_vision_select_layer
+        self.select_feature = getattr(args, "mm_vision_select_feature", "patch")
+
+        if not delay_load:
+            self.load_model()
+        elif getattr(args, "unfreeze_mm_vision_tower", False):
+            self.load_model()
+        else:
+            self.cfg_only = CLIPVisionConfig.from_pretrained(self.vision_tower_name)
+
+    def load_model(self, device_map=None):
+        if self.is_loaded:
+            print(
+                "{} is already loaded, `load_model` called again, skipping.".format(
+                    self.vision_tower_name
+                )
+            )
+            return
+
+        # self.image_processor = CLIPImageProcessor.from_pretrained(
+        #     self.vision_tower_name
+        # )
+
+        tokenizer = BertTokenizer.from_pretrained(config.model.text_encoder.pretrained)
+
+        self.video_processor = VideoMambaVideoProcessor.from_pretrained(
+            self.vision_tower_name
+        )
+
+        model = UMT_VIDEOMAMBA(config=config, tokenizer=tokenizer, is_pretrain=pretrain)
+        model = model.to(torch.device(config.device))
+
+        if config.fp16:
+            if config.get("bf16", True):
+                logger.info("Change to bfloat16 for model")
+                model = model.to(torch.bfloat16)
+            else:
+                logger.info("Change to float16 for model")
+                model = model.half()
+
+        # self.vision_tower = CLIPVisionModel.from_pretrained(
+        #     self.vision_tower_name, device_map=device_map
+        # )
+
+        self.vision_tower = model
+        self.vision_tower.requires_grad_(False)
+
+        self.is_loaded = True
+
+    # def feature_select(self, image_forward_outs):
+    #     image_features = image_forward_outs.hidden_states[self.select_layer]
+    #     if self.select_feature == "patch":
+    #         image_features = image_features[:, 1:]
+    #     elif self.select_feature == "cls_patch":
+    #         image_features = image_features
+    #     else:
+    #         raise ValueError(f"Unexpected select feature: {self.select_feature}")
+    #     return image_features
+
+    def feature_select(self, video_forward_outs):
+        video_features = video_forward_outs.hidden_states[self.select_layer]  # b t n c
+        return video_features  # return all
+
+    @torch.no_grad()
+    def forward(self, images):
+        # if type(images) is list:
+        #     image_features = []
+        #     for image in images:
+        #         image_forward_out = self.vision_tower(
+        #             image.to(device=self.device, dtype=self.dtype).unsqueeze(0),
+        #             output_hidden_states=True,
+        #         )
+        #         image_feature = self.feature_select(image_forward_out).to(image.dtype)
+        #         image_features.append(image_feature)
+        # else:
+        #     image_forward_outs = self.vision_tower(
+        #         images.to(device=self.device, dtype=self.dtype),
+        #         output_hidden_states=True,
+        #     )
+        #     image_features = self.feature_select(image_forward_outs).to(images.dtype)
+
+        # return image_features
+        if type(videos) is list:
+            video_features = []
+            for video in videos:
+                video_forward_out = self.video_tower(
+                    video.to(device=self.device, dtype=self.dtype).unsqueeze(0),
+                    output_hidden_states=True,
+                )
+                video_feature = self.feature_select(video_forward_out).to(video.dtype)
+                video_features.append(video_feature)
+        else:
+            video_forward_outs = self.video_tower(
+                videos.to(device=self.device, dtype=self.dtype),
+                output_hidden_states=True,
+            )
+            video_features = self.feature_select(video_forward_outs).to(videos.dtype)
+
+        return video_features
+
+    # def __init__(self, video_tower, args, delay_load=False, cache_dir='./cache_dir'):
+    #     super().__init__()
+
+    #     self.is_loaded = False
+
+    #     self.video_tower_name = video_tower
+    #     self.select_layer = args.mm_vision_select_layer
+    #     self.select_feature = getattr(args, 'mm_vision_select_feature', 'patch')
+
+    #     self.cache_dir = cache_dir
+
+    #     if not delay_load:
+    #         self.load_model()
+    #     else:
+    #         self.cfg_only = LanguageBindVideoConfig.from_pretrained(self.video_tower_name, cache_dir=self.cache_dir)
+
+    # ############################################################
+    # def load_model(self, device_map=None):
+    #     model = LanguageBindVideo.from_pretrained(self.video_tower_name, cache_dir=self.cache_dir)
+    #     self.video_processor = LanguageBindVideoProcessor(model.config)
+
+    #     # model = LanguageBindImage.from_pretrained('LanguageBind/LanguageBind_Image', cache_dir=self.cache_dir)
+    #     self.video_tower = model.vision_model
+    #     self.video_tower.requires_grad_(False)
+
+    #     self.is_loaded = True
+
+    # def feature_select(self, video_forward_outs):
+    #     video_features = video_forward_outs.hidden_states[self.select_layer]  # b t n c
+    #     return video_features  # return all
+    #     # b, t, n, c = video_features.shape
+    #     # if self.select_feature == 'patch':
+    #     #     video_features = video_features[:, :, 1:]
+    #     # else:
+    #     #     raise ValueError(f'Unexpected select feature: {self.select_feature}')
+    #     # return video_features
+
+    # @torch.no_grad()
+    # def forward(self, videos):
+    #     if type(videos) is list:
+    #         video_features = []
+    #         for video in videos:
+    #             video_forward_out = self.video_tower(video.to(device=self.device, dtype=self.dtype).unsqueeze(0), output_hidden_states=True)
+    #             video_feature = self.feature_select(video_forward_out).to(video.dtype)
+    #             video_features.append(video_feature)
+    #     else:
+    #         video_forward_outs = self.video_tower(videos.to(device=self.device, dtype=self.dtype), output_hidden_states=True)
+    #         video_features = self.feature_select(video_forward_outs).to(videos.dtype)
+
+    #     return video_features
+
+    @property
+    def dummy_feature(self):
+        return torch.zeros(1, self.hidden_size, device=self.device, dtype=self.dtype)
+
+    @property
+    def dtype(self):
+        return self.vision_tower.dtype
+
+    @property
+    def device(self):
+        return self.vision_tower.device
+
+    @property
+    def config(self):
+        if self.is_loaded:
+            return self.vision_tower.config
+        else:
+            return self.cfg_only
+
+    @property
+    def hidden_size(self):
+        return self.config.hidden_size
+
+    @property
+    def num_patches_per_side(self):
+        return self.config.image_size // self.config.patch_size
+
+    @property
+    def num_patches(self):
+        return (self.config.image_size // self.config.patch_size) ** 2

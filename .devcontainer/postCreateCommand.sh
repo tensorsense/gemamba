f07b29818776a86b8f97bb3dd6937e32a66d9661
deepbuzin <andrew@tensorsense.com>
2024-04-17 03:39:02 +0000

    Update devcontainer to properly install local dependencies and tmux

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,10 +1,15 @@
+# install tmux
+
+apt update
+apt install -y tmux
+
 # install Python packages
 
 pip3 install -r requirements.txt
 pip3 install git+https://github.com/facebookresearch/pytorchvideo.git@28fe037d212663c6a24f373b94cc5d478c8c1a1d
 
-pip3 install -e causal-conv1d
-pip3 install -e mamba
+pip3 install -e llava/model/multimodal_encoder/videomamba/causal-conv1d
+pip3 install -e llava/model/multimodal_encoder/videomamba/mamba
 
 nvidia-smi
 

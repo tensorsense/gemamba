1ac998d4fb9e0ec3ba45b742b8bd64c903fc74bc
deepbuzin <andrew@tensorsense.com>
2024-04-26 15:45:42 +0000

    Clean up the training script

--- scripts/custom/pretrain.sh
+++ scripts/custom/pretrain.sh
@@ -3,15 +3,12 @@ cd /data/gemamba/
 export PYTHONPATH=$PYTHONPATH:/data/gemamba/
 export HF_TOKEN=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
 export CUTLASS_PATH=~/cutlass
-# export DS_SKIP_CUDA_CHECK=1
 # export CUDA_VISIBLE_DEVICES=0
 
 deepspeed llava/train/train_mem.py \
     --deepspeed ./scripts/zero2_offload.json \
     --model_name_or_path google/gemma-2b-it \
     --version gemma \
-    --data_path /data/valley/train_json/videochatgpt_tune_fixed.json \
-    --video_folder /data/videochatgpt \
     --vision_tower videomamba \
     --mm_projector_type mlp2x_gelu \
     --mm_vision_select_layer -2 \
@@ -20,16 +17,11 @@ deepspeed llava/train/train_mem.py \
     --image_aspect_ratio pad \
     --group_by_modality_length True \
     --bf16 True \
-    --output_dir ./checkpoints/llava_gemma_mamba_v12_full \
-    --num_train_epochs 1 \
-    --per_device_train_batch_size 24 \
-    --per_device_eval_batch_size 24 \
     --gradient_accumulation_steps 1 \
     --evaluation_strategy "no" \
     --save_strategy "steps" \
-    --save_steps 1000 \
-    --save_total_limit 5 \
-    --learning_rate 3e-5 \
+    --save_steps 5000 \
+    --save_total_limit 1 \
     --weight_decay 0. \
     --warmup_ratio 0.03 \
     --lr_scheduler_type "cosine" \
@@ -37,17 +29,19 @@ deepspeed llava/train/train_mem.py \
     --tf32 True \
     --model_max_length 3072 \
     --gradient_checkpointing True \
+    --per_device_train_batch_size 24 \
+    --per_device_eval_batch_size 24 \
     --dataloader_num_workers 64 \
     --lazy_preprocess True \
     --report_to tensorboard \
-    --cache_dir "./cache_dir"
+    --cache_dir "./cache_dir" \
+    --data_path /data/valley/train_json/videochatgpt_tune_fixed.json \
+    --video_folder /data/videochatgpt \
+    --output_dir ./checkpoints/llava_gemma_mamba_v14_full_valley \
+    --num_train_epochs 1 \
+    --learning_rate 2e-5 \
 
     # --tune_mm_mlp_adapter True \  saves only the adapter into the checkpoint
-    # --data_path /data/valley/train_json/valley_exist.json \
-    # --freeze_backbone True \    
     # --pretrain_mm_mlp_adapter ./checkpoints/llava_gemma_mamba_v4_pt_adapter/mm_projector.bin \
-    # --data_path /data/valley/train_json/videochatgpt_tune_fixed.json \
-    # --video_folder /data/videochatgpt \
-
     # --data_path /data/valley/train_json/valley_exist.json \
     # --video_folder /data/valley \

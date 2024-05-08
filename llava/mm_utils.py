7e76db639639def71d5c32a22f3e87d5238ed66a
deepbuzin <andrew@tensorsense.com>
2024-05-08 08:14:57 +0000

    Remove reduntant tokenization function

--- llava/mm_utils.py
+++ llava/mm_utils.py
@@ -182,34 +182,7 @@ def process_images(images, image_processor, model_cfg):
     return new_images
 
 
-def tokenizer_image_token(prompt, tokenizer, image_token_index=IMAGE_TOKEN_INDEX, return_tensors=None):
-    """
-    <image> special token needs to get inserted separately.
-    However, tokenizer puts <bos> token in the beginning of every chunk, so we need to remove those in order to avoid confusing the model.
-    This also means that the bos token does not need to be inluded in the original prompt.
-    """
-    prompt_chunks = [tokenizer(chunk).input_ids for chunk in prompt.split('<image>')]
-
-    def insert_separator(X, sep):
-        return [ele for sublist in zip(X, [sep]*len(X)) for ele in sublist][:-1]
-
-    input_ids = []
-    offset = 0
-    if len(prompt_chunks) > 0 and len(prompt_chunks[0]) > 0 and prompt_chunks[0][0] == tokenizer.bos_token_id:
-        offset = 1
-        input_ids.append(prompt_chunks[0][0])
-
-    for x in insert_separator(prompt_chunks, [image_token_index] * (offset + 1)):
-        input_ids.extend(x[offset:])
-
-    if return_tensors is not None:
-        if return_tensors == 'pt':
-            return torch.tensor(input_ids, dtype=torch.long)
-        raise ValueError(f'Unsupported tensor type: {return_tensors}')
-    return input_ids
-
-
-def tokenizer_image_token2(
+def tokenizer_image_token(
     prompts, tokenizer, image_token_index=IMAGE_TOKEN_INDEX, return_tensors=None
 ):
     """
